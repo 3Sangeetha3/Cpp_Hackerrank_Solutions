9341b14fb906c42d975f80a2b9f5e27c1d039f39
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2023-09-29 14:43:32 +0530

    Arrays_Introduction.cpp

--- Arrays_Introduction.cpp
+++ Arrays_Introduction.cpp
@@ -11,3 +11,34 @@ int main() {
         cout<<a[i]<<" ";
     return 0;
 }
+
+/*  using class object method
+#include <iostream>
+using namespace std;
+class Array
+{
+    private:
+    int n, a[10];
+    public:
+    void readarr();
+    void getarr();
+};
+void Array::readarr()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+    cin>>a[i];
+}
+void Array::getarr()
+{
+    for(int i=n-1;i>=0;i--)
+    cout<<a[i]<<" ";
+}
+int main()
+{
+    Array a1;
+    a1.readarr();
+    a1.getarr();
+    return 0;
+}
+*/
